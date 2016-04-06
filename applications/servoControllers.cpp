8ed89f49970ed3e769c7ae914768cbca4f48b996
Sebastian Niemann <niemann@sra.uni-hannover.de>
2016-04-06 23:37:23 +0200

    wip

--- applications/servoControllers.cpp
+++ applications/servoControllers.cpp
@@ -76,7 +76,7 @@ void run(
     
     forwards.at(n) = direction;
     servoControllers.run(forwards, speeds);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     servoControllers.stop();
   }
 }
