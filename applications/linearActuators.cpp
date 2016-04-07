5fc6b0cc6433d24a79a8ab9652d088c670e25cf0
Sebastian Niemann <niemann@sra.uni-hannover.de>
2016-04-07 13:37:04 +0200

    wip

--- applications/linearActuators.cpp
+++ applications/linearActuators.cpp
@@ -11,16 +11,23 @@
 bool parseError = false;
 void show_help();
 void run_default();
-void run_calibration();
+void run_single(
+    unsigned int channel,
+    double extension);
+void run_all(
+    double extension);
 void parse_options(
     const int argc,
     const char* argv[]);
 
 int main (const int argc, const char* argv[]) {
   if (argc > 1 && argv[1][0] != '-') {
-    if (std::string(argv[1]) == "calibrate") {
-      parse_options(argc, argv);
-      run_calibration();
+    if (std::string(argv[1]) == "single") {
+      // parse_options(argc, argv);
+      run_single(std::stoi(argv[1]), std::stod(argv[2]));
+    } else if (std::string(argv[1]) == "all") {
+      // parse_options(argc, argv);
+      run_all(std::stod(argv[2]));
     } else {
       ::parseError = true;
       show_help();
@@ -38,7 +45,10 @@ void show_help() {
   std::cout << "  program [options ...]\n";
   std::cout << "    Prints measurements for each sensor until the program is terminated\n";
   std::cout << "\n";
-  std::cout << "  program calibrate [options ...]\n";
+  std::cout << "  program single [options ...]\n";
+  std::cout << "    Starts the sensor calibration\n";
+  std::cout << "\n";
+  std::cout << "  program all [options ...]\n";
   std::cout << "    Starts the sensor calibration\n";
   std::cout << "\n";
   std::cout << "  Options:\n";
@@ -95,8 +105,75 @@ void run_default() {
   }
 }
 
-void run_calibration() {
+void run_single(
+    unsigned int channel,
+    double extension) {
+  ::wiringPiSetupGpio();
   
+  std::vector<demo::Pin> directionPins;
+  switch(channel) {
+    case 0:
+      directionPins.push_back(demo::Gpio::allocatePin(22));
+      break;
+    case 1:
+      directionPins.push_back(demo::Gpio::allocatePin(5));
+      break;
+    case 2:
+      directionPins.push_back(demo::Gpio::allocatePin(6));
+      break;
+    case 3:
+      directionPins.push_back(demo::Gpio::allocatePin(13));
+      break;
+    case 4:
+      directionPins.push_back(demo::Gpio::allocatePin(19));
+      break;
+    case 5:
+      directionPins.push_back(demo::Gpio::allocatePin(26));
+      break;
+  }
+  demo::I2c i2c = demo::Gpio::allocateI2c();
+  demo::ServoControllers servoControllers(std::move(directionPins), std::move(i2c), {channel});
+  
+  demo::Spi spi = demo::Gpio::allocateSpi();
+  demo::ExtensionSensors extensionSensors(std::move(spi), {channel});
+  extensionSensors.setMinimalMeasurableValue(0.1);
+  extensionSensors.setMaximalMeasurableValue(1.0);
+  
+  demo::LinearActuators linearActuators(std::move(servoControllers), std::move(extensionSensors));
+  linearActuators.setMinimalSpeed(0.5);
+  linearActuators.setMaximalSpeed(1.0);
+  linearActuators.setMaximalExtensionDeviation(0.05);
+  
+  linearActuators.setExtensions({extension}, {1.0});
+}
+
+void run_all(
+    double extension) {
+  ::wiringPiSetupGpio();
+  
+  std::vector<demo::Pin> directionPins;
+  directionPins.push_back(demo::Gpio::allocatePin(22));
+  directionPins.push_back(demo::Gpio::allocatePin(5));
+  directionPins.push_back(demo::Gpio::allocatePin(6));
+  directionPins.push_back(demo::Gpio::allocatePin(13));
+  directionPins.push_back(demo::Gpio::allocatePin(19));
+  directionPins.push_back(demo::Gpio::allocatePin(26));
+  demo::I2c i2c = demo::Gpio::allocateI2c();
+  std::vector<unsigned int> i2cChannels = {0, 1, 2, 3, 4, 5};
+  demo::ServoControllers servoControllers(std::move(directionPins), std::move(i2c), i2cChannels);
+  
+  demo::Spi spi = demo::Gpio::allocateSpi();
+  std::vector<unsigned int> spiChannels = {0, 1, 2, 3, 4, 5};
+  demo::ExtensionSensors extensionSensors(std::move(spi), spiChannels);
+  extensionSensors.setMinimalMeasurableValue(0.1);
+  extensionSensors.setMaximalMeasurableValue(1.0);
+  
+  demo::LinearActuators linearActuators(std::move(servoControllers), std::move(extensionSensors));
+  linearActuators.setMinimalSpeed(0.5);
+  linearActuators.setMaximalSpeed(1.0);
+  linearActuators.setMaximalExtensionDeviation(0.05);
+  
+  linearActuators.setExtensions(std::vector<double>(6, extension), {1.0, 1.0, 1.0, 1.0, 1.0, 1.0});
 }
 
 void parse_options(
