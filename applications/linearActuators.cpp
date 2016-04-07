6f2b5a75558e5dca210a2d1d3433c37fa7878c0d
Sebastian Niemann <niemann@sra.uni-hannover.de>
2016-04-07 08:57:28 +0200

    wip

---
+++ applications/linearActuators.cpp
@@ -0,0 +1,96 @@
+// C++ standard library
+#include <chrono>
+#include <thread>
+
+// WiringPi
+#include <wiringPi.h>
+
+// Demonstrator
+#include <demonstrator>
+
+bool parseError = false;
+void show_help();
+void run_default();
+void run_calibration();
+void parse_options(
+    const int argc,
+    const char* argv[]);
+
+int main (const int argc, const char* argv[]) {
+  if (argc > 1 && argv[1][0] != '-') {
+    if (std::string(argv[1]) == "calibrate") {
+      parse_options(argc, argv);
+      run_calibration();
+    } else {
+      ::parseError = true;
+      show_help();
+    }    
+  } else {
+    parse_options(argc, argv);
+    run_default();
+  }
+  
+  return parseError ? 1 : 0;  
+}
+
+void show_help() {
+  std::cout << "Usage:\n";
+  std::cout << "  program [options ...]\n";
+  std::cout << "    Prints measurements for each sensor until the program is terminated\n";
+  std::cout << "\n";
+  std::cout << "  program calibrate [options ...]\n";
+  std::cout << "    Starts the sensor calibration\n";
+  std::cout << "\n";
+  std::cout << "  Options:\n";
+  std::cout << "         --verbose    Prints additional (debug) information\n";
+  std::cout << "    -h | --help       Displays this help\n";
+  std::cout << std::flush;
+}
+
+void run_default() {
+  ::wiringPiSetupGpio();
+  
+  std::vector<demo::Pin> directionPins;
+  directionPins.push_back(demo::Gpio::allocatePin(22));
+  directionPins.push_back(demo::Gpio::allocatePin(5));
+  directionPins.push_back(demo::Gpio::allocatePin(6));
+  directionPins.push_back(demo::Gpio::allocatePin(13));
+  directionPins.push_back(demo::Gpio::allocatePin(19));
+  directionPins.push_back(demo::Gpio::allocatePin(26));
+  demo::I2c i2c = demo::Gpio::allocateI2c();
+  std::vector<unsigned int> i2cChannels = {0, 1, 2, 3, 4, 5};
+  
+  demo::Spi spi = demo::Gpio::allocateSpi();
+  std::vector<unsigned int> spiChannels = {0, 1, 2, 3, 4, 5};
+  
+  demo::LinearActuators linearActuators(std::move(directionPins), std::move(i2c), i2cChannels, std::move(spi), spiChannels);
+  linearActuators.setMaximalExtensionDeviation(0.05);
+  
+  while(1) {
+    std::vector<bool> extensions = {0.5, 0.5, 0.5, 0.5, 0.5, 0.5};
+    std::vector<double> maximalSpeeds = {1.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    
+    linearActuators.setExtensions(extensions, maximalSpeeds);
+  }
+}
+
+void run_calibration() {
+  
+}
+
+void parse_options(
+    const int argc,
+    const char* argv[]) {
+  for (std::size_t n = 1; n < argc; ++n) {
+    std::string option = argv[n];
+    
+    if ((option == "-h") || (option == "--help")) {
+      show_help();
+    } else if (option == "--verbose") {
+      ::demo::isVerbose = true;
+    } else {
+      ::parseError = true;
+      show_help();
+    }
+  }
+}
