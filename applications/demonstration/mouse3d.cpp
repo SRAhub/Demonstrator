a0bdb2091b53b2c33d75824a0003ad8ca35b2bab
Sebastian Niemann <niemann@sra.uni-hannover.de>
2016-04-26 18:39:01 +0200

    wip

--- applications/demonstration/mouse3d.cpp
+++ applications/demonstration/mouse3d.cpp
@@ -29,12 +29,12 @@ int main (int argc, char **argv) {
     arma::Row<double>::fixed<8> mouse3d = mouse3dSensors.measure();    
     for (size_t n = 0; n < 3; n++) {
       if (std::abs(mouse3d(n)) > 0.5) {
-        endEffectorPose += std::copysign(0.005, mouse3d(n));
+        endEffectorPose(n) += std::copysign(0.005, mouse3d(n));
       }
     }
 
     for (size_t n = 0; n < motorPis.size(); n++) {
-      network.send(motorPis.at(n), 31415, "set " + vectorToString(endEffectorPose / motorPis.size()));
+      network.send(motorPis.at(n), 31415, "set " + vectorToString(endEffectorPose));
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   };
